ce39b000f492c5b26bdfaedd9cd0752c552dcda0
Tim Kimpton <tkimpton@users.noreply.github.com>
2014-06-27 07:53:51 +0100

    Update JamfHelperWarning.sh

--- Bash/JamfHelperWarning.sh
+++ Bash/JamfHelperWarning.sh
@@ -5,7 +5,7 @@ MSG='ENTER
 YOUR VALUE'
 
 sudo /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper \
--windowType utility -title "WARNING" -description "$MSG" -icon /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertCautionIcon.icns -iconSize 96 -button1 "OK"
+-windowType utility -title "WARNING" -description "$MSG" -icon /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertCautionIcon.icns -iconSize 96 -button1 "OK" -defaultButton 1
 
 
 
