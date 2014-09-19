96a15a54f327a0dda9939fe67108719ec9d851be
Tim Kimpton <tkimpton@users.noreply.github.com>
2014-09-19 07:22:16 +0100

    Update SelfService_DeleteKeyChain.sh

--- Bash/SelfService_DeleteKeyChain.sh
+++ Bash/SelfService_DeleteKeyChain.sh
@@ -4,13 +4,13 @@
 user=`ls -l /dev/console | cut -d " " -f4`
 
 # Show the Desktop through Expose
-/Applications/Utilities/Expose.app/Contents/MacOS/Expose 1
+/Applications/Mission\ Control.app/Contents/MacOS/Mission\ Control 1
 
 # Display the dialog to the user
 x=`/usr/bin/osascript <<EOT
 tell application "Finder"
         activate
-        set myReply to button returned of (display dialog "This will delete your keychain login and everything in it. You will then need to log off and log in again! Click Cancel to save you work and then you need to run this again or click Do it! to continue and wait for confirmation!" buttons {"More Info" , "Cancel" , "Do it!"} default button "Cancel")
+        set myReply to button returned of (display dialog "This will delete your keychain login and everything in it. YOUR MACHINE WILL THEN RESTART! Click Cancel to abort" buttons {"More Info" , "Cancel" , "Do it!"} default button "Cancel")
 end tell
 EOT`
 echo 'Button is: '$x
@@ -18,40 +18,45 @@ echo 'Button is: '$x
 if [[ $x = "More Info" ]]; then
 
 # Open the solutions page
-su - "${user}" -c 'open -a /Applications/Firefox.app http://servicedesk/SolutionsHome.do'
+su - "${user}" -c 'open http://support.apple.com/kb/TS5362'
+
+# Pause 5 seconds
+sleep 5
+
+su - "${user}" -c 'open http://support.apple.com/kb/HT1631'
 
 # Pause 10 seconds
 sleep 10
 
-
-# Opening the solution as the user
-su - "${user}" -c 'open -a /Applications/Firefox.app http://servicedesk/AddSolution.do?solID=17'
-
-else
-
-if [[ $x = "Do it!" ]]; then
+elif [[ $x = "Do it!" ]]; then
 echo 'Shell script continues...'
 
-# Delete the login.keychain as the currently logged in user
-su - "${user}" -c 'rm -rf ~/Library/Keychains/login.keychain'
+# Delete the keychains as the currently logged in user
+su - "${user}" -c 'rm -rf ~/Library/Keychains/*'
 
 # Show the Desktop through Expose
-/Applications/Utilities/Expose.app/Contents/MacOS/Expose 1
+/Applications/Mission\ Control.app/Contents/MacOS/Mission\ Control 1
 
 # Displaying a message to tell the user it has finished
-osascript -e 'tell app "System Events" to display dialog "Keychain deleted. You will need to log out and log in again!"'
+osascript -e 'tell app "System Events" to display dialog "Keychain deleted. You will need to restart your machine"'
 
 # Pause 5 seconds
 sleep 5
 
 # Log out
-/usr/bin/osascript <<-EOF
+shutdown -r now
 
-tell application "System Events"
-log out
+else
+
+# Displaying a message to tell the user it has finished
+osascript -e 'tell app "System Events" to display dialog "Nothing Done. Aborted!"'
+
+/usr/bin/osascript <<EOF
+try
+tell application "Finder"
+activate
 end tell
-
+end try
 EOF
 
-fi
 fi
