e161e62c1b0170bd5a128dce937650dbf443bfe0
Tim Kimpton <tkimpton@users.noreply.github.com>
2015-04-01 09:12:20 +0100

    Update and rename Enable_QuickLookSelection.sh to Enable_Quick_Look_Selection.sh

--- Bash/Enable_Quick_Look_Selection.sh
+++ Bash/Enable_Quick_Look_Selection.sh
@@ -4,7 +4,7 @@
 user=`ls -l /dev/console | cut -d " " -f4`
 
 # Make quick look better by making text selectable
-su - "${user}" -c 'defaults write com.apple.finder QLEnableTextSelection -bool TRUE'
+defaults write com.apple.finder QLEnableTextSelection -bool TRUE
 
 # Add to the new user template
 #sudo defaults write /System/Library/User\ Template/English.lproj/Library/Preferences/com.apple.finder QLEnableTextSelection -bool true
