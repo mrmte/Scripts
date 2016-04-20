c3d87c3b69fdcbd346ea98580ff0febb5317f246
Tim Kimpton <tkimpton@users.noreply.github.com>
2016-04-20 19:05:41 +0100

    Update Disable_Sleep_and_ScreenSaver.sh

--- Bash/Disable_Sleep_and_ScreenSaver.sh
+++ Bash/Disable_Sleep_and_ScreenSaver.sh
@@ -8,9 +8,9 @@ user=`ls -l /dev/console | cut -d " " -f4`
 
 ##### DO NOT MODIFY BELOW THIS LINE #####
 
-# unload the launch agent if it already exist
-if [ -f /Library/LaunchAgents/com.caffeinate.Launchd.plist ]; then
-su "${user}" -c 'launchctl unload /Library/LaunchAgents/com.caffeinate.Launchd.plist'
+# unload if it already exist
+if [ -f /Library/LaunchDaemons/com.sn.caffeinate.Launchd.plist ]; then
+su "${user}" -c 'launchctl unload /Library/LaunchDaemons/com.sn.caffeinate.Launchd.plist'
 fi
 
 # Check to see if the binary exists and if so create the launch agent
@@ -20,21 +20,24 @@ echo "<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-	<key>Label</key>
-	<string>com.caffeinate.Launchd</string>
-	<key>ProgramArguments</key>
-	<array>
-		<string>/usr/bin/caffeinate</string>
-		<string>-d</string>
-		<string>-i</string>
-		<string>-m</string>
-	</array>
-	<key>RunAtLoad</key>
-	<true/>
+        <key>Label</key>
+        <string>com.sn.caffeinate.Launchd</string>
+        <key>ProgramArguments</key>
+        <array>
+                <string>/usr/bin/caffeinate</string>
+                <string>-d</string>
+                <string>-i</string>
+                <string>-m</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
 </dict>
-</plist>" >/Library/LaunchAgents/com.caffeinate.Launchd.plist
+</plist>" >/Library/LaunchDaemons/com.sn.caffeinate.Launchd.plist
+chown root:wheel /Library/LaunchDaemons/com.sn.caffeinate.Launchd.plist
+chmod 644 /Library/LaunchDaemons/com.sn.caffeinate.Launchd.plist
 
+sleep 3
 
 # Load the launch agent
-su "${user}" -c 'launchctl load /Library/LaunchAgents/com.caffeinate.Launchd.plist'
+launchctl load /Library/LaunchDaemons/com.sn.caffeinate.Launchd.plist
 fi
