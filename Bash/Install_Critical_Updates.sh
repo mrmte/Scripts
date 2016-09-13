fa739cf3b75b7e11604b0a1d8ec638d9eb51e0a6
Tim Kimpton <tkimpton@users.noreply.github.com>
2016-09-13 15:36:56 +0100

    Update Install_Critical_Updates.sh

--- Bash/Install_Critical_Updates.sh
+++ Bash/Install_Critical_Updates.sh
@@ -9,8 +9,21 @@ getosupd=$(softwareupdate -l | grep OSXUpd | awk 'NR==1 {print $2}')
 # Get any security updates
 getsecupd=$(softwareupdate -l | grep SecUpd | awk 'NR==1 {print $2}')
 
+
+### DO NOT MODIFY BELOW THIS LINE ###
+
+if  [[ $getsecupd="No new software available." ]] && [[ $getosupd="No new software available." ]]; then
+
+echo "no os or security updates to apply"
+
+exit 1
+
+else
+
 # Install OS updates
 softwareupdate -i $getosupd
 
 # Install Security updates
 softwareupdate -i $getsecupd
+
+fi
