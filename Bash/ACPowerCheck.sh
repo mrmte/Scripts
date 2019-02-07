9e1454fdaa3670e335873d11590811106e7d5fd3
Tim Kimpton <tkimpton@users.noreply.github.com>
2019-02-07 10:20:39 +0000

    Update ACPowerCheck.sh

--- Bash/ACPowerCheck.sh
+++ Bash/ACPowerCheck.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-# check if machine is running on power
+function AC_POWER_CHECK() {
 
-# If machine is a Laptop and is running on Battery, go through and loop the message before continuing
+# check if machine is running on power
 while [[ $(pmset -g ps | head -1 | cut -d "'" -f 2)  != "AC Power" ]]; do
 
 # Display the message
-osascript -e 'tell app "System Events" to display dialog "You need to connect the machine to AC Power to continue" buttons {"OK"} default button 1'
+
+if [ -e /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper ]; then
+        /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -icon /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertStopIcon.icns -heading "ERROR" -description "You must connect the machine to ac power." -button1 "OK"
+
+else
+/usr/bin/osascript -e 'tell app "System Events" to display dialog "You need to connect the machine to AC Power to continue" buttons {"OK"} default button 1'
 done
+
+fi
+
+done
+
+}
+
+AC_POWER_CHECK
+
+exit 0
+
